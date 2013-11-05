0d95795ec12287b2a3ca22faad9879054912738f
Armen <armenn@vmn.net>
2013-11-05 13:13:40 -0500

    quit flood fix

--- plugins/jsrun.js
+++ plugins/jsrun.js
@@ -7,12 +7,12 @@ var run = {
             sandbox.run(code, function(out) {
                 var log = out.result;
                 if(log.length > 80) {
-                    log.length = 80;
+                    log = log.substr(0, 80);
                     log += "[...]";
                 }
                 var cons = "console: " + out.console.join("\n");
                 if(cons.length > 80) {
-                    cons.length = 80;
+                    cons = cons.substr(0, 80);
                     cons += "[...]";
                 }
                 run.bot.say(msg.channel, log);
